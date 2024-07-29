852eb133605ddb956f1b7e3b67e373203a3ef4f6
Phat <minhphatnguyen6@gmail.com>
2024-07-29 21:42:31 +0700

    Initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/phatnguyen138/go_api/internal/server"
+)
+
+func main() {
+
+	server := server.NewServer()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(fmt.Sprintf("cannot start server: %s", err))
+	}
+}
